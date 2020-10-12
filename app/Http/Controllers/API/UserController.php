46a28fb43c8556ce4e9caf764f0c10021240670a
a.emam@eramint.com <a.emam@eramint.com>
2020-10-12 15:38:48 +0200

    first commit in bestpractise

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,22 +17,21 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'c_password' => 'required|same:password',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
-        }
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
-        $success['name'] =  $user->name;
+
+        $this->user->create($input);
+
+
+        $success['token'] =  $this->user->createToken('MyApp')->accessToken;
+        $success['name'] =  $this->user->name;
         return response()->json(['success'=>$success], $this->successStatus);
     }
 
@@ -44,12 +43,7 @@ class UserController extends Controller
 
     public function login(Request $request){
 
-        $login = $request->validate([
-            'email'=>'required|string',
-            'password'=>'required',
-        ]);
-
-        if(Auth::attempt($login)){
+        if(Auth::attempt($request)){
             $user = Auth::user();
             $success['token'] =  $user->createToken('MyApp')-> accessToken;
             return response()->json(['success' => $success], $this-> successStatus);
---
+++ app/Http/Requests/StoreUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUser extends FormRequest
+{
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ];
+
+    }
+}
